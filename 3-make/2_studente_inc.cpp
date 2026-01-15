452eb1d379aeee8d8bcbf398f707e0cb1782f4c1
tommaso-bonora <bonora2008tommaso@gmail.com>
2026-01-15 09:32:31 +0100

    Update 2_studente_inc.cpp

--- 3-make/2_studente_inc.cpp
+++ 3-make/2_studente_inc.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 /***********************************************************************************************************
- *                                 Modifica della classe studente (incapsulamento)                         *     
- *   Per come abbiamo definito la classe nel file precedente, è possibile inserire una media di 44:  *         
- *   questo è concettualmente sbagliato, ma il programma non ci darà nessun errore se lo facciamo          *     
- *   Per evitare problemi di questo tipo, si preferisce                                                    *         
- *      1. modificare la "visibilità" dei campi della classe rendendoli privati                            *     
- *      2. creare funzioni che permettono di fare modifiche controllate ai campi,                          * 
- *         (ad esempio in questo caso controllando che la nuova media sia tra 0 e 10)                      * 
+ *                                 Modifica della classe studente (incapsulamento)                         *
+ *   Per come abbiamo definito la classe nel file precedente, è possibile inserire una media di 44:  *
+ *   questo è concettualmente sbagliato, ma il programma non ci darà nessun errore se lo facciamo          *
+ *   Per evitare problemi di questo tipo, si preferisce                                                    *
+ *      1. modificare la "visibilità" dei campi della classe rendendoli privati                            *
+ *      2. creare funzioni che permettono di fare modifiche controllate ai campi,                          *
+ *         (ad esempio in questo caso controllando che la nuova media sia tra 0 e 10)                      *
  *                                                                                                         */
 
 class Studente {
@@ -38,13 +38,37 @@ public:
     void stampaStudente(){
         cout << "Nome: " << nome << ", eta: " << eta << ", media: " << media << endl;
     }
+
+    string getNome(){
+    return nome;
+    }
+     int getEta(){
+    return eta;
+    }
+     float getMedia(){
+    return media;
+    }
+
+
+
+
 };
 
 int main()
 {
+  Studente s;
+  s.setNome("Mario");
+  s.setEta(17);
+  s.setMedia(8.00);
+
+s.stampaStudente();
+
+
+
+
     // TODO Istanziare un nuovo oggetto e valorizzarlo attraverso i nuovi metodi set
 
-    // TODO Adesso che gli attributi della classe sono privati, com'è possibile accedervi? 
+    // TODO Adesso che gli attributi della classe sono privati, com'è possibile accedervi?
     //      Risolvere il problema creando nuove funzioni "get" che permettano l'accesso a tutti gli attributi della classe
 
     return 0;
